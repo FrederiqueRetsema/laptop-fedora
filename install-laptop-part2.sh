3a114c9c8ecb45a42353e25621ef4a656f5ad016
Frederique Retsema <fretsema@fretsema.nl>
2024-03-24 14:57:41 +0100

    Added Microsoft Teams

--- install-laptop-part2.sh
+++ install-laptop-part2.sh
@@ -4,6 +4,7 @@ git clone https://github.com/FrederiqueRetsema/Xforce-unpublished.git
 git clone https://github.com/FrederiqueRetsema/aws-bitwarden-pvt.git
 echo 'Manual steps:'
 echo '- WiFi settings > Gear icon > IPv4 > DNS 8.8.8.8 8.8.4.4 1.1.1.1'
+echo '- Tweaks > Mouse and Touchpad > Area'
 echo '- Install bitwarden'
 echo '- Install ublock origin'
 echo '- Check chromecast'
